6deba4422f99a952c9d90d597534cfc1e5e82a3f
Егор Смирнов <egorsmirn@outlook.com>
2023-03-29 13:35:16 +0400

    Add postgre support

---
+++ SystemElectric.TestTask.Postgre/Repository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SystemElectric.TestTask.Domain.Args;
+using SystemElectric.TestTask.Domain.Repositories;
+using SystemElectric.TestTask.Postgre.Context;
+
+namespace SystemElectric.TestTask.Postgre.Repository
+{
+    public sealed class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly MainContext _context;
+
+        public GenericRepository(MainContext context)
+        {
+            _context = context;
+        }
+
+        public event EventHandler<TEntityArgs<T>>? OnAddedEntry;
+
+        public async Task AddEntry(T entry, CancellationToken cancellationToken)
+        {
+            _context.Set<T>().Add(entry);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            OnAddedEntry?.Invoke(this, new TEntityArgs<T>(entry));
+        }
+
+        public async Task<IEnumerable<T>> GetEntries(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+        {
+            return await _context.Set<T>().Where(predicate).ToArrayAsync(cancellationToken);
+        }
+    }
+}
---
+++ SystemElectric.TestTask.Postgre/RepositoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SystemElectric.TestTask.Domain.Entities;
+using SystemElectric.TestTask.Domain.Repositories;
+using SystemElectric.TestTask.Postgre.Context;
+using SystemElectric.TestTask.Postgre.Repository;
+
+namespace SystemElectric.TestTask.Postgre
+{
+    public static class RepositoryExtensions
+    {
+        public static void AddNpgSqlRepository(this IServiceCollection services, string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MainContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+            services.AddTransient(context => new MainContext(optionsBuilder.Options));
+            services.AddSingleton<IGenericRepository<CarEntry>, GenericRepository<CarEntry>>();
+            services.AddSingleton<IGenericRepository<DriverEntry>, GenericRepository<DriverEntry>>();
+        }
+    }
+}
